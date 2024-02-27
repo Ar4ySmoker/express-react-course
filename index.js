a3db4e7e8bcd5320bc4b5ce1053bd35f2390bb38
Artur Fedcenco <developerar4y@gmail.com>
2024-02-27 09:12:32 +0200

    1

--- index.js
+++ index.js
@@ -5,22 +5,25 @@ import fileUpload from 'express-fileupload';
 import cors from 'cors'
 
 const PORT = 5600;
-const DB_URL = `mongodb+srv://alihtgolt:6MCABoHvpwZ5oNU1@cluster0.tzoti0i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const DB_URL = `mongodb+srv://developerar4y:Ar4ycool@cluster0.m3budbf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-const app = express()
+const app = express();
 app.use(cors());
-app.use(express.json())
-app.use(express.static('static'))
-app.use(fileUpload({}))
-app.use('/api', router)
+app.use(express.json());
+app.use(express.static("static"));
+app.use(fileUpload({}));
+app.use("/api", router);
 
 async function startApp() {
-    try {
-        await mongoose.connect(DB_URL, {useUnifiedTopology: true, useNewUrlParser: true})
-        app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT))
-    } catch (e) {
-        console.log(e)
-    }
+  try {
+    await mongoose.connect(DB_URL, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    app.listen(PORT, () => console.log("SERVER STARTED ON PORT " + PORT));
+  } catch (e) {
+    console.log(e);
+  }
 }
-
+console.log("work");
 startApp()
